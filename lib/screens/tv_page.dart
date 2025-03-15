9f9e9e9d2cefb766dcd849f1a0163e4000f0d621
KimpiegamesYT1 <broek.floris@gmail.com>
2025-03-15 18:55:46 +0100

    TV Pagina native gemaakt.

--- lib/screens/tv_page.dart
+++ lib/screens/tv_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:video_player/video_player.dart';
 
 class TVPage extends StatefulWidget {
   const TVPage({super.key});
@@ -10,7 +9,84 @@ class TVPage extends StatefulWidget {
 }
 
 class _TVPageState extends State<TVPage> {
+  late VideoPlayerController _controller;
   bool _isError = false;
+  bool _isLoading = true;
+  
+  // List of stream URLs to try in order of preference
+  final List<String> _streamUrls = [
+    'https://server-67.stream-server.nl:1936/Samen1TV/Samen1TV/playlist.m3u8', // HLS stream
+    'https://server-67.stream-server.nl:2000/tunein/Samen1TV/v/m3u8', // VLC stream
+    'rtsp://server-67.stream-server.nl/Samen1TV/Samen1TV', // RTSP stream
+  ];
+  
+  int _currentStreamIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeVideoPlayer();
+  }
+
+  void _initializeVideoPlayer() {
+    setState(() {
+      _isLoading = true;
+      _isError = false;
+    });
+
+    if (_currentStreamIndex >= _streamUrls.length) {
+      setState(() {
+        _isError = true;
+        _isLoading = false;
+      });
+      return;
+    }
+
+    final streamUrl = _streamUrls[_currentStreamIndex];
+    
+    _controller = VideoPlayerController.networkUrl(
+      Uri.parse(streamUrl),
+      httpHeaders: {'User-Agent': 'Samen1TV-App'},
+      formatHint: VideoFormat.hls, // Set the format hint for HLS streams
+    )
+      ..initialize().then((_) {
+        // Ensure the first frame is shown and play video
+        if (mounted) {
+          setState(() {
+            _isLoading = false;
+            _controller.play();
+          });
+        }
+      }).catchError((error) {
+        print("Failed to initialize player: $error");
+        // Try the next stream URL if this one fails
+        _currentStreamIndex++;
+        _controller.dispose();
+        _initializeVideoPlayer();
+      })
+      ..addListener(() {
+        if (_controller.value.hasError) {
+          print("Video player error: ${_controller.value.errorDescription}");
+          if (mounted && !_isError) {
+            setState(() => _isError = true);
+          }
+        }
+      });
+  }
+
+  void _retryWithNextStream() {
+    _currentStreamIndex++;
+    if (_controller.value.isInitialized) {
+      _controller.dispose();
+    }
+    _initializeVideoPlayer();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,30 +102,141 @@ class _TVPageState extends State<TVPage> {
             aspectRatio: 16 / 9,
             child: _isError
                 ? Center(
-                    child: ElevatedButton(
-                      onPressed: () => launchUrl(
-                        Uri.parse('https://www.twitch.tv/samen1_events'),
-                        mode: LaunchMode.externalApplication,
-                      ),
-                      child: const Text('Kijk op Twitch'),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Kon de video niet laden',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        const SizedBox(height: 12),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Try again with current stream
+                            setState(() => _isError = false);
+                            if (_controller.value.isInitialized) {
+                              _controller.dispose();
+                            }
+                            _initializeVideoPlayer();
+                          },
+                          child: const Text('Probeer opnieuw'),
+                        ),
+                        const SizedBox(height: 8),
+                        if (_currentStreamIndex < _streamUrls.length - 1)
+                          TextButton(
+                            onPressed: _retryWithNextStream,
+                            child: const Text('Probeer alternatieve stream'),
+                          ),
+                      ],
                     ),
                   )
-                : InAppWebView(
-                    initialData: InAppWebViewInitialData(data: '''
-                      <html>
-                        <body style="margin:0">
-                          <video style="width:100%;height:100%" controls autoplay>
-                            <source src="https://server-67.stream-server.nl:2000/VideoPlayer/Samen1TV" type="video/mp4">
-                          </video>
-                        </body>
-                      </html>
-                    '''),
-                    onReceivedError: (controller, request, error) {
-                      setState(() => _isError = true);
-                    },
-                  ),
+                : _isLoading
+                    ? const Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            CircularProgressIndicator(),
+                            SizedBox(height: 16),
+                            Text('Stream laden...'),
+                          ],
+                        ),
+                      )
+                    : _controller.value.isInitialized
+                        ? Stack(
+                            alignment: Alignment.bottomCenter,
+                            children: [
+                              VideoPlayer(_controller),
+                              VideoProgressIndicator(
+                                _controller,
+                                allowScrubbing: true,
+                                padding: const EdgeInsets.all(8),
+                              ),
+                              _VideoControls(controller: _controller),
+                            ],
+                          )
+                        : const Center(child: CircularProgressIndicator()),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _VideoControls extends StatefulWidget {
+  final VideoPlayerController controller;
+
+  const _VideoControls({required this.controller});
+
+  @override
+  State<_VideoControls> createState() => _VideoControlsState();
+}
+
+class _VideoControlsState extends State<_VideoControls> {
+  bool _showControls = true;
+  late bool _isPlaying;
+
+  @override
+  void initState() {
+    super.initState();
+    _isPlaying = widget.controller.value.isPlaying;
+    widget.controller.addListener(_updateState);
+    
+    // Auto-hide controls after 3 seconds
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted && _isPlaying) {
+        setState(() => _showControls = false);
+      }
+    });
+  }
+
+  void _updateState() {
+    if (mounted) {
+      setState(() {
+        _isPlaying = widget.controller.value.isPlaying;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.controller.removeListener(_updateState);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() => _showControls = !_showControls);
+        
+        // Auto-hide controls after 3 seconds if playing
+        if (_showControls && _isPlaying) {
+          Future.delayed(const Duration(seconds: 3), () {
+            if (mounted && _isPlaying) {
+              setState(() => _showControls = false);
+            }
+          });
+        }
+      },
+      child: AnimatedOpacity(
+        opacity: _showControls ? 1.0 : 0.0,
+        duration: const Duration(milliseconds: 300),
+        child: Container(
+          color: Colors.black26,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: Icon(_isPlaying ? Icons.pause : Icons.play_arrow),
+                color: Colors.white,
+                iconSize: 32,
+                onPressed: () {
+                  _isPlaying ? widget.controller.pause() : widget.controller.play();
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import flutter_local_notifications
 import package_info_plus
 import shared_preferences_foundation
 import url_launcher_macos
+import video_player_avfoundation
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -18,5 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
