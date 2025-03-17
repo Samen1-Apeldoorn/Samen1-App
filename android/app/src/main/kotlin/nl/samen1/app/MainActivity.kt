6d20b11ce04225a665be74d5d988fe07a752fd7a
KimpiegamesYT1 <broek.floris@gmail.com>
2025-03-17 21:58:04 +0100

    Another Important bug Fix

--- android/app/src/main/kotlin/nl/samen1/app/MainActivity.kt
+++ android/app/src/main/kotlin/nl/samen1/app/MainActivity.kt
@@ -1,13 +1,7 @@
 package nl.samen1.app
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import androidx.annotation.NonNull
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        GeneratedPluginRegistrant.registerWith(flutterEngine)
-    }
+    // No custom configuration needed
 }
