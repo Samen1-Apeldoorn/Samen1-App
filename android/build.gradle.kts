4c36a94ef91cba80c578508429cd66836c55ccfa
KimpiegamesYT1 <broek.floris@gmail.com>
2025-04-05 23:55:22 +0200

    Version Updates

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,13 +15,13 @@ subprojects {
     // Add test dependencies for Android projects
     plugins.withId("com.android.library") {
         dependencies {
-            "testImplementation"("org.robolectric:robolectric:4.10.3")
-            "testImplementation"("androidx.test:core:1.5.0")
+            "testImplementation"("org.robolectric:robolectric:4.14.1")
+            "testImplementation"("androidx.test:core:1.6.1")
             "testImplementation"("androidx.test.ext:junit:1.2.1")
-            "testImplementation"("org.mockito:mockito-core:5.4.0")
-            "testImplementation"("org.mockito:mockito-android:5.4.0")
-            "testImplementation"("net.bytebuddy:byte-buddy:1.14.5")
-            "testImplementation"("net.bytebuddy:byte-buddy-agent:1.14.5")
+            "testImplementation"("org.mockito:mockito-core:5.17.0")
+            "testImplementation"("org.mockito:mockito-android:5.17.0")
+            "testImplementation"("net.bytebuddy:byte-buddy:1.17.5")
+            "testImplementation"("net.bytebuddy:byte-buddy-agent:1.17.5")
         }
         
         tasks.withType<Test> {
